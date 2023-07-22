91f9716a2912a3f01565c417bde0403cfd56fecf
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2023-07-23 01:46:40 +0700

    🎉 custom menu 2

--- electron/contextMenu.js
+++ electron/contextMenu.js
@@ -13,9 +13,29 @@ module.exports = (win) =>
 					.resize({ width: 16 }),
 				type: 'normal',
 			},
-			actions.separator(), // dấu ngăn cách
-			actions.copy(),
-			actions.paste(),
+			{
+        type: 'separator',
+      },
+			{
+				label: 'Copy',
+				accelerator: 'CmdOrCtrl+C',
+				role: 'copy',
+				icon: nativeImage
+					.createFromPath(path.join(__dirname, 'Icon.png'))
+					.resize({ width: 16 }),
+				visible: props.isEditable || props.selectionText.length > 0,
+        enabled: props.editFlags.canCopy && props.selectionText.length > 0,
+			},
+			{
+				label: 'Paste',
+				accelerator: 'CmdOrCtrl+V',
+				role: 'paste',
+				icon: nativeImage
+					.createFromPath(path.join(__dirname, 'Icon.png'))
+					.resize({ width: 16 }),
+				visible: props.isEditable || props.selectionText.length > 0,
+				enabled: props.editFlags.canPaste,
+			},
 			// Docs: https://www.electronjs.org/docs/latest/api/menu-item/
 		],
 	});
